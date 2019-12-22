f3357daa42164350490666c957044d8dd3235a85
Emilio <erogalla@utexas.edu>
2019-12-21 18:33:55 -0600

    created post API

--- backend/toDo/todoList/api/urls.py
+++ backend/toDo/todoList/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import messageListView
+from .views import messageListView, postMessageView 
 
 urlpatterns = [
     path('', messageListView.as_view()),
+    path('create/', postMessageView.as_view()),
 ]
--- backend/toDo/todoList/api/views.py
+++ backend/toDo/todoList/api/views.py
@@ -1,7 +1,11 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView
 from todoList.models import ToDoList
 from .serializers import listSerializer
 
 class messageListView(ListAPIView):
+    serializer_class = listSerializer
+    queryset = ToDoList.objects.all()
+
+class postMessageView(CreateAPIView):
     serializer_class = listSerializer
     queryset = ToDoList.objects.all()
